75135e6be816a959b3dd4254a43e3e7be5a4ca30
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2022-10-20 14:50:24 -0400

    Programei a opção 1 saudação

--- AulinhaGit/Program.cs
+++ AulinhaGit/Program.cs
@@ -1,14 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("É impressão minha ou o mundo está ficando cada vez mais louco??\n\n>>SIM\n>>NÃO\n");
-string resposta = Console.ReadLine();
-Console.Clear();
-resposta = resposta.ToUpper();
-if (resposta == "SIM")
-{
-    Console.WriteLine("HAHAHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHA\nHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHA\nHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHA\nHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHA\n");
-}
-else
-{
-    Console.WriteLine("?");
-}
+Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-");
+Console.WriteLine("[1] - Saudação.");
+Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-");
 
+Console.Write("Escolha uma opção: ");
+int opc = Convert.ToInt32(Console.ReadLine());
+
+if (opc == 1)
+{
+    Console.Clear();
+    Console.WriteLine("Qual é o seu nome?");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine("\nOlá, " + nome + "!");
+}
