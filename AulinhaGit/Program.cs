97f0dd8f238739307c60c8f10bb83ef515bf8abd
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2022-10-20 14:20:57 -0400

    Adicionar arquivos de projeto.

---
+++ AulinhaGit/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Samuel é estranho?");
+string resposta = Console.ReadLine();
+Console.Clear();
+
+if (resposta == "sim")
+{
+    Console.WriteLine("eu sabia");
+}
+else
+{
+    Console.WriteLine("é mentira");
+}
+
